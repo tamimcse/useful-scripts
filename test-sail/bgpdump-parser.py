135416ba9c718377386dda00d9e80a7859e4ee31
tamimcse <tamim@csebuet.org>
2018-11-20 23:06:07 -0500

    Finished the BGPdump parser. It reads BGPdump -m output and an AS ID. It outputs the routing table for the AS ID

--- test-sail/bgpdump-parser.py
+++ test-sail/bgpdump-parser.py
@@ -6,31 +6,17 @@ out = open("fib.txt", "a")
 prefix_set = set()
 nh_dict = {}
 i = 0
-filter_by = 3549
+filter_by = 34288
 
-with open("bgpdump-m.txt") as f:
+with open("rib.linx.20141217.0000") as f:
     for line in f:
 	arr = line.replace("\r\n", "").split("|")
-	#4-th element contains the AS
-        as_id = arr[4]
-	#5-th element contains prefix and prefix length
-	prefix = arr[5].split("/")[0]
-	#5-th element contains the AS path
-        as_path = arr[6]
-	as_list = as_path.split(" ")
+        as_id = arr[4]#Extract the AS
+	prefix = arr[5]#Extract the prefix
+	next_hop = arr[8]
 
 	if int(as_id) != filter_by :
 	    continue    	 
-
-	if len(as_list) > 1 :
-	    next_hop = as_list[1]
-        elif len(as_list) == 1 :
-	    #The next-hop is within the AS	
-	    next_hop = 0
-#            print line 		
-        else :
-	    next_hop = -1
-            print "Something went wrong"
 
 	if prefix not in prefix_set :
 	    prefix_set.add(prefix)
@@ -38,4 +24,6 @@ with open("bgpdump-m.txt") as f:
             if next_hop not in nh_dict :
 	        nh_dict[next_hop] = i
                 i = i + 1
+		print i
 	    out.write("{0}\tveth{1}\n".format(arr[5], nh_dict[next_hop]%32))
+#	    out.write("{0}\t{1}\n".format(arr[5], next_hop))
