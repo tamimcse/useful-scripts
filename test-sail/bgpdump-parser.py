87ce0333d3431c5aecdf3cd127402349444a4a19
tamimcse <tamim@csebuet.org>
2018-11-21 17:11:05 -0500

    .

--- test-sail/bgpdump-parser.py
+++ test-sail/bgpdump-parser.py
@@ -3,21 +3,23 @@
 
 import sys
 
-out = open("routes.txt", "a")
-
 prefix_set = set()
 nh_dict = {}
 i = 0
 
-if len(sys.argv) != 3 :
-	print "sudo python bgpdump-parser.py bgpdump peer"
+if len(sys.argv) != 4 :
+	print "sudo python bgpdump-parser.py inputfile peer outputfile"
 	print "where"
-	print "bgpdump   ---	Output of BGPdump -m option "
-	print "peer      ---	AS ID of the peer"
+	print "inputfile  ---	Output of BGPdump -m option "
+	print "peer       ---	AS ID of the peer"
+	print "outputfile ---	AS ID of the peer"
 	sys.exit()
 
 bgpdump_output_file = sys.argv[1] 
 target_peer_id = sys.argv[2]
+outputfile = sys.argv[3]
+
+out = open(outputfile, "w")
 
 with open(bgpdump_output_file) as f:
     for line in f:
@@ -35,6 +37,6 @@ with open(bgpdump_output_file) as f:
             if next_hop not in nh_dict :
 	        nh_dict[next_hop] = i
                 i = i + 1
-		print i
+		print "number of next-hops = {0}".format(i)
 	    out.write("{0}\tveth{1}\n".format(arr[5], nh_dict[next_hop]%32))
 #	    out.write("{0}\t{1}\n".format(arr[5], next_hop))
--- test-sail/bgpdump-parser.sh
+++ test-sail/bgpdump-parser.sh
@@ -1,1 +1,8 @@
-sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  3549
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  3549 rv2.20181105.1000-3549-link3
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  18106 rv2.20181105.1000-18106-viewqwest
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  40191 rv2.20181105.1000-40191-zerofail
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  3356 rv2.20181105.1000-3356-CenturyLink
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  2914 rv2.20181105.1000-2914-nttamerica
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  6453 rv2.20181105.1000-6453-tata
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  7922 rv2.20181105.1000-7922-comcast
+sudo python bgpdump-parser.py rv2.20181105.1000-bgpdump-m-output  6939 rv2.20181105.1000-6939-HurricaneElectric 
