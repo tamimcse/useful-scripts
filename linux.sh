8650118184ff55835bb016a22f84a0c522bee717
tamimcse <tamim@csebuet.org>
2019-06-01 16:59:05 -0400

    .

--- linux.sh
+++ linux.sh
@@ -67,6 +67,11 @@ sudo apt-get install -y libreadline-dev &&
 sudo apt-get install -y iverilog gtkwave yosys &&
 #Scala
 sudo apt-get install -y scala &&
+#Install Scala sbt
+echo "deb https://dl.bintray.com/sbt/debian /" | sudo tee -a /etc/apt/sources.list.d/sbt.list &&
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2EE0EA64E40A89B84B2DF73499E82A75642AC823 &&
+sudo apt-get update -y &&
+sudo apt-get install -y sbt &&
 #Set path variables for Domino
 echo 'export PATH="$PATH:/home/tamim/sketch-1.6.9/sketch-frontend"' >> .bashrc &&
 echo 'export SKETCH_HOME="/home/tamim/sketch-1.6.9/sketch-frontend/runtime"' >> .bashrc &&
