aa4c85890243f0dd8e5c81aa3fdb1f2480fd20f9
tamimcse <tamim@csebuet.org>
2020-02-23 20:59:15 -0500

    .

--- install_ubuntu_18.sh
+++ install_ubuntu_18.sh
@@ -51,7 +51,7 @@ sudo apt-get install -y nvidia-361 nvidia-prime &&
 sudo apt-get install -y lsscsi cpuset sysstat rt-tests libnuma-dev hwloc &&
 sudo apt-get install -y libvirt-bin libvirt-dev &&
 #DVD burner
-sudo apt-get install -y k3b &&
+sudo apt-get install -y k3b brasero &&
 #Installing GStreamer
 sudo apt-get install -y libgstreamer1.0-0 gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-doc gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio &&
 sudo apt-get install -y python3-pip &&
