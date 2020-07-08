cdd4f6c57ba31eb1b278e21fc8e22eac7a1a28f5
tamimcse <tamim@csebuet.org>
2020-07-08 19:03:02 -0400

    .

--- install_ubuntu_18.sh
+++ install_ubuntu_18.sh
@@ -218,6 +218,8 @@ sudo chmod a+rwx zoom_amd64.deb &&
 sudo apt-get install -y ./zoom_amd64.deb &&
 #install Discord
 sudo snap install discord --classic &&
+#install Docker
+sudo snap install docker &&
 wget https://www.syntevo.com/downloads/smartgit/smartgit-19_1_6.deb &&
 sudo chmod a+rwx -R smartgit-19_1_6.deb &&
 sudo apt install ./smartgit-19_1_6.deb &&
