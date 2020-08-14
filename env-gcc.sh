c56fcc527799029b9829ac4258484279fa326c3f
tamimcse <tamim@csebuet.org>
2020-08-14 15:34:13 -0400

    l

---
+++ env-gcc.sh
@@ -0,0 +1,3 @@
+#Run this script manually before compiling gcc
+unset LIBRARY_PATH CPATH C_INCLUDE_PATH PKG_CONFIG_PATH CPLUS_INCLUDE_PATH INCLUDE
+sudo ln -s /usr/include/asm-generic /usr/include/asm
