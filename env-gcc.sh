9a566ea74fdfac2b6cebf3b68bcbc4a097f2c7f7
tamimcse <tamim@csebuet.org>
2020-08-15 17:56:46 -0400

    .

--- env-gcc.sh
+++ env-gcc.sh
@@ -2,3 +2,5 @@
 unset LIBRARY_PATH CPATH C_INCLUDE_PATH PKG_CONFIG_PATH CPLUS_INCLUDE_PATH INCLUDE
 #sudo ln -s /usr/include/asm-generic /usr/include/asm
 sudo ln -s /usr/include/x86_64-linux-gnu/asm /usr/include/asm
+../configure --prefix=/usr  --enable-languages=c,c++
+
