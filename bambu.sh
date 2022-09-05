dec1f0fadde59d0303132b7b58cd9b0b3b3c09fb
tamimcse <tamim@csebuet.org>
2022-09-04 17:30:56 -0700

    .

--- bambu.sh
+++ bambu.sh
@@ -1,2 +1,3 @@
+#These scripts adds Bambu and Vivado's executables to the PATH derectory. Without them, the script will fail.
 export PATH=../../src:../../../src:/opt/panda/bin:$PATH
 export PATH=/tools/Xilinx/Vivado/2022.1/bin/:$PATH
