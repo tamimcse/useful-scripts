587df3ed1672f1ca3706ee4c65d07b722bda79e0
tamimcse <mislam4@kent.edu>
2023-01-29 20:07:27 -0800

    .

--- install_ubuntu_20.sh
+++ install_ubuntu_20.sh
@@ -137,6 +137,8 @@ sudo apt-get install -y iverilog gtkwave libmotif-dev &&
 sudo apt-get install -y python3-virtualenv verilator &&
 #Needed for sv2v
 sudo apt-get install -y haskell-stack &&
+#Needed for Dsagen
+sudo apt install -y zsh &&
 #Needed for Bambu HLS
 sudo apt-get install -y autoconf autoconf-archive automake libtool g++ gcc-7 g++-7 gcc-8 g++-8 gcc-7-plugin-dev  gcc-8-plugin-dev gcc-7-multilib gcc-8-multilib g++-7-multilib g++-8-multilib gfortran-7 gfortran-7-multilib gfortran-8 gfortran-8-multilib libclang-6.0-dev clang-6.0 libclang-6.0-dev clang-7 libclang-7-dev clang-8 libclang-8-dev clang-9 libclang-9-dev clang-10 libclang-10-dev clang-11 libclang-11-dev clang-12 libclang-12-dev libbdd-dev libboost-all-dev libmpc-dev libmpfr-dev libxml2-dev liblzma-dev libmpfi-dev zlib1g-dev libicu-dev bison doxygen flex graphviz iverilog verilator make libsuitesparse-dev libglpk-dev &&
 pip install defusedxml &&
